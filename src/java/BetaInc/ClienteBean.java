97e79ff9143b51f515a8838c225f8f738334607e
DELL7567-NT\Winder Rezende <windergt@gmail.com>
2018-11-30 16:59:06 -0200

    Atualização do Projeto

--- src/java/BetaInc/ClienteBean.java
+++ src/java/BetaInc/ClienteBean.java
@@ -1,66 +1,98 @@
 package BetaInc;
 
+import Persistencia.Arquivo;
 import Util.Formatar;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
 
 /**
  * @Autor m159255
  * @Data 08/10/2018
  */
 @ManagedBean
-@SessionScoped
+@RequestScoped
 public class ClienteBean {
-    
+
     private String nome;
-    private String dtNacimento;
+    private String dtNascimento;
     private String cpf;
     private String rg;
     private String endereco;
     private double qtdReais;
     private List<ClienteBean> cliente = new ArrayList<>();
-    
+    Arquivo arquivo;
+
     public ClienteBean() {
+        arquivo = new Arquivo("empresas.txt");
+        this.Obter();
     }
 
-    public ClienteBean(String nome, String dtNacimento, String cpf, String rg, String endereco, double qtdReais) {
+    public ClienteBean(String nome, String dtNascimento, String cpf, String rg, String endereco, double qtdReais) {
         this.nome = nome;
-        this.dtNacimento = dtNacimento;
+        this.dtNascimento = dtNascimento;
         this.cpf = cpf;
         this.rg = rg;
         this.endereco = endereco;
         this.qtdReais = qtdReais;
     }
 
-    public void add(){
-        ClienteBean novo = new ClienteBean(this.nome, Formatar.Data(this.dtNacimento, "yyyy-MM-dd", "dd/MM/yyyy"), this.cpf, this.rg, endereco, qtdReais);
+    public void add() {
+        ClienteBean novo = new ClienteBean(this.nome, Formatar.Data(this.dtNascimento, "yyyy-MM-dd", "dd/MM/yyyy"), this.cpf, this.rg, this.endereco, this.qtdReais);
         cliente.add(novo);
         nome = "";
-        dtNacimento = null;
+        dtNascimento = "";
         cpf = "";
         rg = "";
         endereco = "";
         qtdReais = 0.0;
+        Salvar();
     }
-    
-    public void editar(ClienteBean c){
+
+    public void editar(ClienteBean c) {
         nome = c.getNome();
-        dtNacimento = Formatar.Data(c.getDtNacimento(), "dd/MM/yyyy", "yyyy-MM-dd");
+        dtNascimento = Formatar.Data(c.getDtNascimento(), "dd/MM/yyyy", "yyyy-MM-dd");
         cpf = c.getCpf();
         rg = c.getRg();
         endereco = c.getEndereco();
         qtdReais = c.getQtdReais();
         cliente.remove(c);
+        Salvar();
     }
-    
-    public void remover(ClienteBean c){
+
+    public void remover(ClienteBean c) {
         cliente.remove(c);
+        Salvar();
+    }
+
+    private void Obter() {
+        arquivo.Ler("@Beta;");
+        for (int i = 0; i < Arquivo.Objeto.size(); i++) {
+            String linha = Arquivo.Objeto.get(i);
+            String col[] = linha.split(Arquivo.Separador);
+            ClienteBean novo = new ClienteBean(col[1], col[2], col[3], col[4], col[5], Double.parseDouble(col[6]));
+            cliente.add(novo);
+        }
+    }
+
+    private void Salvar() {
+        for (int i = 0; i < cliente.size(); i++) {
+            ClienteBean c = cliente.get(i);
+            String conteudo = 
+                    "@Beta" + Arquivo.Separador 
+                    + c.getNome() + Arquivo.Separador 
+                    + c.getDtNascimento() + Arquivo.Separador 
+                    + c.getCpf() + Arquivo.Separador 
+                    + c.getRg() + Arquivo.Separador 
+                    + c.getEndereco() + Arquivo.Separador 
+                    + c.getQtdReais();
+            Arquivo.dados.add(conteudo);
+        }
+        arquivo.Salvar();
     }
 
     //Getters e Setters
-
     public String getNome() {
         return nome;
     }
@@ -69,12 +101,12 @@ public class ClienteBean {
         this.nome = nome;
     }
 
-    public String getDtNacimento() {
-        return dtNacimento;
+    public String getDtNascimento() {
+        return dtNascimento;
     }
 
-    public void setDtNacimento(String dtNacimento) {
-        this.dtNacimento = dtNacimento;
+    public void setDtNascimento(String dtNascimento) {
+        this.dtNascimento = dtNascimento;
     }
 
     public String getCpf() {
@@ -115,5 +147,13 @@ public class ClienteBean {
 
     public void setCliente(List<ClienteBean> cliente) {
         this.cliente = cliente;
+    }
+
+    public Arquivo getArquivo() {
+        return arquivo;
+    }
+
+    public void setArquivo(Arquivo arquivo) {
+        this.arquivo = arquivo;
     }
 }
--- src/java/BetaInc/VendedorBean.java
+++ src/java/BetaInc/VendedorBean.java
@@ -1,38 +1,58 @@
 package BetaInc;
 
+import Util.Formatar;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 
 /**
  * @Autor m159255
  * @Data 08/10/2018
  */
 @ManagedBean
-@RequestScoped
+//@RequestScoped
+@SessionScoped
 public class VendedorBean {
     private String nome;
-    private Date dtNacimento;
+    private String dtNascimento;
     private String endereco;
     private List<ClienteBean> cliente = new ArrayList<>();
-    private List<ClienteBean> vendedor = new ArrayList<>();
+    private List<VendedorBean> vendedor = new ArrayList<>();
     
     
     public VendedorBean() {
     }
+
+    public VendedorBean(String nome, String dtNascimento, String endereco, List<ClienteBean> cliente) {
+        this.nome = nome;
+        this.dtNascimento = dtNascimento;
+        this.endereco = endereco;
+        this.cliente = cliente;
+    }
     
     public void add(){
-    
+        VendedorBean novo = new VendedorBean(this.nome, Formatar.Data(this.dtNascimento, "yyyy-MM-dd", "dd/MM/yyyy"), this.endereco, this.cliente);
+        vendedor.add(novo);
+        nome = "";
+        dtNascimento = "";
+        cliente = null;
+        
     }
     
-    public void editar(){
-    
+    public void editar(VendedorBean v){
+        nome = v.getNome();
+        dtNascimento = Formatar.Data(v.getDtNascimento(), "dd/MM/yyyy", "yyyy-MM-dd");
+        endereco = v.getEndereco();
+        vendedor = null;
+        vendedor.remove(v);
     }
     
-    public void remover(){
-    
+    //Getters e Setters
+    public void remover(VendedorBean v){
+        vendedor.remove(v);
     }
 
     public String getNome() {
@@ -43,12 +63,12 @@ public class VendedorBean {
         this.nome = nome;
     }
 
-    public Date getDtNacimento() {
-        return dtNacimento;
+    public String getDtNascimento() {
+        return dtNascimento;
     }
 
-    public void setDtNacimento(Date dtNacimento) {
-        this.dtNacimento = dtNacimento;
+    public void setDtNascimento(String dtNascimento) {
+        this.dtNascimento = dtNascimento;
     }
 
     public String getEndereco() {
@@ -67,11 +87,11 @@ public class VendedorBean {
         this.cliente = cliente;
     }
 
-    public List<ClienteBean> getVendedor() {
+    public List<VendedorBean> getVendedor() {
         return vendedor;
     }
 
-    public void setVendedor(List<ClienteBean> vendedor) {
+    public void setVendedor(List<VendedorBean> vendedor) {
         this.vendedor = vendedor;
     }
 }
